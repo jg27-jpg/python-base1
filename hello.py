556301ed6bd5c29d223de83ef1a5ccc7624050ab
jg27-jpg <creepesc@gmail.com>
2026-02-25 20:42:19 -0300

    Novas coisas

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print("Hello, World!")
+print("Hello, World!!") 
+print("Welcome to Python programming.")
+print("This is a simple Python script to demonstrate basic syntax.")
+print("Python is a versatile language used for various applications.")
